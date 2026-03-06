ca17c0416c9b2ba51c10797a1c6dc619312cc326
kishorre-rp <kishorrerp@gmail.com>
2026-03-06 21:09:24 +0530

    Task-1

---
+++ Assignment-6/Task1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Task1 {
+	
+	 static class CompanySystem{
+		int EmployeeID;
+		String EmployeeName;
+		
+		
+		CompanySystem(){
+			EmployeeID = 100;
+		    EmployeeName = "Not Assigned";  
+		
+	}
+	 }
+	   public static void main(String[] args) {
+		   
+		   CompanySystem  cs = new CompanySystem();
+		   System.out.println("The name is: "+cs.EmployeeName+" with the ID is:" +cs.EmployeeID);
+			   
+	   }
+		   
+	   }
+	
