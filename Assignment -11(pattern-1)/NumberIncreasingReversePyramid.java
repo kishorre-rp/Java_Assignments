6a6d7c61cd9f5d114e0b7408f2a1b806285a5c33
kishorre-rp <kishorrerp@gmail.com>
2026-03-18 20:53:05 +0530

    Task-3

--- Assignment -11(pattern-1)/NumberIncreasingReversePyramid.java
+++ Assignment -11(pattern-1)/NumberIncreasingReversePyramid.java
@@ -8,8 +8,10 @@ public class Number_incresaing_prymaid {
 		System.out.print("Enter the number of rows: ");
 		int row = sc.nextInt();
 		
-		for(int i=1;i<=row;i++) {
+		for(int i=1;i<=row;i++) { 
+
 			for(int j =1;j<=row-i+1;j++) {
+
 				System.out.print(j+" ");
 			}
 			System.out.println();
