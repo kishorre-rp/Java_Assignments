22b5d4be5d6f248c98efff8c4ea9d5967846b02d
kishorre-rp <kishorrerp@gmail.com>
2026-03-19 22:20:01 +0530

    Tasks

---
+++ Pattern_1/Number_Changing_pyramid.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Number_Changing_pyramaid {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows: ");
+		int row = sc.nextInt();
+		int n = 1;
+		
+		for(int i=1;i<=row;i++) {
+			for(int j =1;j<=i;j++) {
+				System.out.print(n +" ");
+				n++;
+			}
+			System.out.println();
+		}
+	}
+}
