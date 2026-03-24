00bf04ff4f2acf9c38df4d009f48027059c4f351
kishorre-rp <kishorrerp@gmail.com>
2026-03-24 10:02:41 +0530

    Task-6

---
+++ Pattern_2/Task6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Task5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows:  ");
+		int n = sc.nextInt();
+
+		int size = 2 * n - 1;
+
+		for (int i = 1; i <= size; i++) {
+			int row = (i <= n) ? i : size - i + 1;
+
+			for (int j = 1; j <= row - 1; j++) {
+				System.out.print(" ");
+			}
+			int width = 2 * (n - row) + 1;
+
+			for (int j = 1; j <= width; j++) {
+
+				if (row == 1 || j == 1 || j == width) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
