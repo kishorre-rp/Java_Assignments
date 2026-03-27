642945333e10e8134a362b96e1af8180ef5fb2bd
kishorre-rp <kishorrerp@gmail.com>
2026-03-27 19:38:32 +0530

    Task-2

---
+++ Assignment_Exception/Task2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Task2 {
+
+	public static void main(String[] args) {
+	
+		
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter user name: ");
+			String name = sc.nextLine();
+			
+			if(name == "Kishorre") {
+				System.out.println();			}
+			
+		}catch(Exception e) {
+			
+		}
+	}
+}
