e3bc7989253c3749a1c52ebe3ddd8b58939c81d5
kishorre-rp <kishorrerp@gmail.com>
2026-04-14 20:06:53 +0530

    Task-4

---
+++ Array_List_Assignment/Task4.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		arr.add(20);
+		arr.add(40);
+		arr.add(60);
+		arr.add(80);
+		arr.add(100);
+		
+		System.out.println(arr);
+		
+	for(int arrs : arr) {
+		if() {
+			
+		}
+		
+	}
+	}
+}
