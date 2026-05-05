6f35be1ac442bdb6f164a6f8d140a079b15a94b4
kishorre-rp <kishorrerp@gmail.com>
2026-05-05 20:11:12 +0530

    Tasks Completed

--- String_Assignment/Task3.java
+++ String_Assignment/Task3.java
@@ -1,30 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Task3 {
-	public static String reverse(String str) {
-
-		if (str == null)
-			return null;
-
-		char[] chars = str.toCharArray();
-		int left = 0, right = chars.length - 1;
-
-		while (left < right) {
-			char temp = chars[left];
-			chars[left] = chars[right];
-			chars[right] = temp;
-			left++;
-			right--;
-		}
-		return new String(chars);
-	}
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the String to reverse: ");
-		String input = sc.nextLine();
-		System.out.println("Input: " + input);
-		System.out.println(" output is: reverse of given String; " + reverse(input));
-
+		System.out.print("Enter the word to reverse:  ");
+		String str = sc.nextLine();
+		String rev = "";
+		 
+		 for(int i=str.length()-1;i>=0;i--) {
+			 
+			 rev += str.charAt(i);
+		 }System.out.println(rev);
 	}
 }
